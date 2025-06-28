a3647cf81707b86031adf1c8769d0401dd6c08b0
Кристина Барменкова <barmenkova.kristina@mail.ru>
2025-06-28 16:52:07 +0300

    feat: реализована функция выхода и добавлена индикация загрузки

--- src/components/profile-menu/profile-menu.tsx
+++ src/components/profile-menu/profile-menu.tsx
@@ -1,11 +1,42 @@
-import { FC } from 'react';
-import { useLocation } from 'react-router-dom';
+import { FC, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+
 import { ProfileMenuUI } from '@ui';
 
+import { logoutApi } from '@api';
+import { deleteCookie } from '../../utils/cookie';
+
+import { useDispatch } from '../../services/store';
+import { logout } from '../../services/slices/userSlice';
+
 export const ProfileMenu: FC = () => {
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const handleLogout = () => {};
+  const handleLogout = () => {
+    setIsLoading(true);
 
-  return <ProfileMenuUI handleLogout={handleLogout} pathname={pathname} />;
+    logoutApi()
+      .then(() => {
+        localStorage.removeItem('refreshToken');
+        deleteCookie('accessToken');
+        dispatch(logout());
+        navigate('/login', { replace: true });
+      })
+      .catch((err) => setError(err))
+      .finally(() => setIsLoading(false));
+  };
+
+  return (
+    <>
+      <ProfileMenuUI
+        handleLogout={handleLogout}
+        pathname={pathname}
+        isLoading={isLoading}
+      />
+    </>
+  );
 };
